2a4a4caaf835009a0cac2746928cb8bf6020c6d0
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-21 22:45:59 -0500

    Delete and Save buttons working

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = process.env.PORT || 8080
-
+const { json } = require('express');
+const express = require('express');
+const { fstat } = require('fs');
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 8080;
+const db = require('./db/db.json');
+// const fs = require('fs')
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) =>{
+    return res.json(db)
+})
+
+app.post('/api/notes', (req,res) => {
+    const newNote = req.body
+    newNote.id = Math.floor((Math.random() * 100) + 1)
+    db.push(newNote)
+    console.log(db)
+    return res.json(newNote)
+
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    const id = req.params.id
+    for(var i = 0; i < db.length; i++) {
+        if(db[i].id == id) {
+            db.splice(i, 1);
+            break;
+        }
+    }
+    return res.json(db)
+  });
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`)
 })
+
